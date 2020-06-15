0db6016d7843a3102b3fddcb96f40cf94c8e61ba
Nimduli <nimduli@datampowered.com.au>
2020-06-15 16:33:29 +1000

    Initial commit - V1 2020/06/15

--- core_classes/comma_separated_text_handling.py
+++ core_classes/comma_separated_text_handling.py
@@ -1,7 +1,44 @@
-
 class comma_separated_handling_class():
 
-    def __init__(self ,df):
-        print('comma_separated_handling_class')
+    def __init__(self, df, column_name):
+        self.df = df
+        self.column_name = column_name
+
+    # removes the commas in each number and returns the edited dataset
+    def remove_commas(self):
+        count = 0   # counter for reference of element index in the respective column
+        for value in self.df[self.column_name]:
+            value = self.convert_to_string(value)
+
+            # identify if the current element contains a comma
+            if self.locate_commas(value):
+                value = value.replace(",", "")   # substitute all the commas to blank spaces
+                value = self.convert_to_float(value)
+                self.df[self.column_name].iat[count] = value   # use element index to substitute new value
+                print(self.df[self.column_name].iloc[count])
+
+            count = count + 1
+
+        return self.df
+
+    # identifies if an element contains a comma or not and returns true or false respectively
+    def locate_commas(self, value):
+        character = ','
+        if character in value:
+            return True
+        else:
+            return False
+
+    # converts the given value to float
+    def convert_to_float(self, value):
+        final = float(value)
+        return final
+
+    # converts the given value to string
+    def convert_to_string(self, value):
+        final = str(value)
+        return final
+
+
 
 
--- testing/test.py
+++ testing/test.py
@@ -1,17 +1,17 @@
 from core_classes.comma_separated_text_handling import comma_separated_handling_class
 import pandas as pd
-
-
-
+import numpy as np
 
 
 #getting data from the data_files folder
-df=pd.read_excel('data_files/insurance_data_file.xlsx')
-print(df)
-
+df=pd.read_excel('insurance_data_file.xlsx', sheet_name='Policy Statistics')
 
 
 #class testing starts
-class_obj=comma_separated_handling_class(df)
+class_obj=comma_separated_handling_class(df, 'Value')
+df = class_obj.remove_commas()
+print(df['Value'])
+
+
 
 
