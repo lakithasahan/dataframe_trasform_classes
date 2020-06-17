f35329d311efbc8c729a2facc8614ae4b2e69f9f
Nimduli <nimduli@datampowered.com.au>
2020-06-17 10:01:17 +1000

    Initial commit - V2 2020/06/17

--- core_classes/comma_separated_text_handling.py
+++ core_classes/comma_separated_text_handling.py
@@ -1,25 +1,33 @@
 class comma_separated_handling_class():
 
-    def __init__(self, df, column_name):
+    def __init__(self, df):
         self.df = df
-        self.column_name = column_name
+
+    def main(self):
+        list_of_columns = list(self.df.columns)
+
+        print(list_of_columns)  # testing code
+
+        for column in list_of_columns:
+            self.remove_commas(column)
+
+        return self.df
 
     # removes the commas in each number and returns the edited dataset
-    def remove_commas(self):
+    def remove_commas(self, column_name):
         count = 0   # counter for reference of element index in the respective column
-        for value in self.df[self.column_name]:
+        for value in self.df[column_name]:
             value = self.convert_to_string(value)
 
             # identify if the current element contains a comma
             if self.locate_commas(value):
-                value = value.replace(",", "")   # substitute all the commas to blank spaces
-                value = self.convert_to_float(value)
-                self.df[self.column_name].iat[count] = value   # use element index to substitute new value
-                print(self.df[self.column_name].iloc[count])
+                new_value = value.replace(",", "")   # substitute all the commas to blank spaces
+                value = self.convert_to_float(new_value, value)
+                self.df[column_name].iat[count] = value   # use element index to substitute new value
+
+                print(self.df[column_name].iloc[count])  # testing code
 
             count = count + 1
-
-        return self.df
 
     # identifies if an element contains a comma or not and returns true or false respectively
     def locate_commas(self, value):
@@ -30,9 +38,13 @@ class comma_separated_handling_class():
             return False
 
     # converts the given value to float
-    def convert_to_float(self, value):
-        final = float(value)
-        return final
+    def convert_to_float(self, new_value, value):
+        try:
+            final = float(new_value)
+            return final
+        except:
+            print('Not a number')  # testing code
+            return value
 
     # converts the given value to string
     def convert_to_string(self, value):
--- testing/test.py
+++ testing/test.py
@@ -3,14 +3,22 @@ import pandas as pd
 import numpy as np
 
 
-#getting data from the data_files folder
-df=pd.read_excel('insurance_data_file.xlsx', sheet_name='Policy Statistics')
+# getting data from the data_files folder
+df = pd.read_excel('insurance_data_file.xlsx', sheet_name='Policy Statistics')
+
+# class testing starts
+class_obj = comma_separated_handling_class(df)
+df = class_obj.main()
+
+# testing code
+for value in df['Value']:
+    print(value)
+    print(type(value))
 
 
-#class testing starts
-class_obj=comma_separated_handling_class(df, 'Value')
-df = class_obj.remove_commas()
-print(df['Value'])
+
+
+
 
 
 
