895e1fd6e1fa2652ab316f5c35d44ce7c8a60941
Nimduli <nimduli@datampowered.com.au>
2020-06-22 13:13:59 +1000

    Initial commit - V3 2020/06/22

--- core_classes/lat_and_log_handling.py
+++ core_classes/lat_and_log_handling.py
@@ -1,12 +1,19 @@
 from opencage.geocoder import OpenCageGeocode
+import csv
+import geocoder
+import pandas as pd
+import googlemaps
+
 class lat_and_log_handling_class():
 
     def __init__(self, df):
         self.df = df
 
     def main(self):
+
         list_lat = []
         list_lng = []
+        #g_key = googlemaps.Client(key = "AIzaSyC6rR2go5mkA3BsPBJh21s2y_lQX_qCSA4")
 
         key = "203effefcc474b74b30dc0f52cb82ea8"
         geocoder = OpenCageGeocode(key)
@@ -19,9 +26,12 @@ class lat_and_log_handling_class():
             results = geocoder.geocode(query)
             lat = results[0]['geometry']['lat']
             lng = results[0]['geometry']['lng']
-            print(lat + ', ' + lng)
+            print(query + '  ' + str(lat) + ', ' + str(lng))
             list_lat.append(lat)
             list_lng.append(lng)
+
         return self.df
+
+
 
 
--- testing/test.py
+++ testing/test.py
@@ -1,19 +1,25 @@
-from core_classes.comma_separated_text_handling import comma_separated_handling_class
+from core_classes.lat_and_log_handling import  lat_and_log_handling_class
 import pandas as pd
 import numpy as np
-
+from opencage.geocoder import OpenCageGeocode
+import plotly.express as px
 
 # getting data from the data_files folder
-df = pd.read_excel('insurance_data_file.xlsx', sheet_name='Policy Statistics')
+df = pd.read_csv(r'C:\Users\N\Documents\covid_19.csv')
 
 # class testing starts
-class_obj = comma_separated_handling_class(df)
+class_obj = lat_and_log_handling_class(df)
 df = class_obj.main()
 
-# testing code
-for value in df['Value']:
-    print(value)
-    print(type(value))
+fig = px.scatter_mapbox(df, lat="lat", lon="l"
+                                           "ng", color_discrete_sequence=["fuchsia"], zoom=3, height=300)
+fig.update_layout(mapbox_style="open-street-map")
+fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+fig.show()
+
+
+
+
 
 
 
